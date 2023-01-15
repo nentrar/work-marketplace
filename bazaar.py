f2bb0ce9b3281e054157051522d05a0492edd789
nentrar <nentrar.mail@gmail.com>
2023-01-15 17:02:34 +0100

    Add method to get card img

--- bazaar.py
+++ bazaar.py
@@ -1,4 +1,5 @@
 import json
+from mtgsdk import Card
 
 
 class Bazaar:
@@ -9,13 +10,13 @@ class Bazaar:
     def read_all_data(self):
         return self.data
 
-    def add_new_item(self, category, topic, img, price):
+    def add_new_item(self, category, card_name, set_name, price):
         current_count = len(self.data) + 1
         new_item = {
             f"{current_count}": {
                 "Cat": category,
-                "Card": topic,
-                "Image": img,
+                "Card": card_name,
+                "Image": self.get_card_image(card_name, set_name),
                 "Price": price
             }
         }
@@ -23,3 +24,8 @@ class Bazaar:
         current_data.update(new_item)
         with open('items.json', 'w') as self.items:
             json.dump(current_data, self.items)
+
+    @staticmethod
+    def get_card_image(card_name, set_name):
+        card_data = Card.where(name=card_name).where(set_name=set_name).all()
+        return card_data[0].image_url
