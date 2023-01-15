16176d20942f2e864ccfcee4d50fa6b4ebabc0ef
nentrar <nentrar.mail@gmail.com>
2023-01-15 16:27:29 +0100

    Add method for updating json of new item

--- marketplace.py
+++ marketplace.py
@@ -3,8 +3,23 @@ import json
 
 class Marketplace:
     def __init__(self):
-        with open('items.json') as items:
-            self.data = json.load(items)
+        with open('items.json') as self.items:
+            self.data = json.load(self.items)
 
     def read_all_data(self):
         return self.data
+
+    def add_new_item(self, category, topic, img, price):
+        current_count = len(self.data) + 1
+        new_item = {
+            f"{current_count}": {
+                "Cat": category,
+                "Card": topic,
+                "Image": img,
+                "Price": price
+            }
+        }
+        current_data = self.data
+        current_data.update(new_item)
+        with open('items.json', 'w') as self.items:
+            json.dump(current_data, self.items)
